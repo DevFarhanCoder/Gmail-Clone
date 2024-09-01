0f7e4f69d38cb402be1f40a0cc34f36977baf186
DevFarhanCoder <154915648+DevFarhanCoder@users.noreply.github.com>
2024-09-01 09:21:28 +0530

    Update api.urls.js

--- client/src/services/api.urls.js
+++ client/src/services/api.urls.js
@@ -2,27 +2,27 @@ const BASE_URL = 'https://gmailclone-2024.vercel.app/'; // Change this to your a
 
 export const API_URLS = {
     saveSentEmails: {
-        endpoint: `${BASE_URL}/api/emails/save`, // Full endpoint URL
+        endpoint: `${BASE_URL}/emails/save`, // Full endpoint URL
         method: 'POST'
     },
     saveDraftEmails: {
-        endpoint: `${BASE_URL}/api/emails/save-draft`, // Full endpoint URL
+        endpoint: `${BASE_URL}/emails/save-draft`, // Full endpoint URL
         method: 'POST'
     },
     getEmailFromType: {
-        endpoint: `${BASE_URL}/api/emails`, // Full endpoint URL
+        endpoint: `${BASE_URL}/emails`, // Full endpoint URL
         method: 'GET'
     },
     toggleStarredMails: {
-        endpoint: `${BASE_URL}/api/emails/starred`, // Full endpoint URL
+        endpoint: `${BASE_URL}/emails/starred`, // Full endpoint URL
         method: 'POST'
     },
     deleteEmails: {
-        endpoint: `${BASE_URL}/api/emails/delete`, // Full endpoint URL
+        endpoint: `${BASE_URL}/emails/delete`, // Full endpoint URL
         method: 'DELETE'
     },
     moveEmailsToBin: {
-        endpoint: `${BASE_URL}/api/emails/bin`, // Full endpoint URL
+        endpoint: `${BASE_URL}/emails/bin`, // Full endpoint URL
         method: 'POST'
     }
 };
